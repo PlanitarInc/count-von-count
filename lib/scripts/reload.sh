d5ea7ab4338a43efb06a464c5bfa047e452206fa
korya <korya@github.com>
2014-11-03 15:23:55 -0500

    Fix nginx reload script

--- lib/scripts/reload.sh
+++ lib/scripts/reload.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-DEPLOY_TO="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../.."
+
+set -e
+
+CVC_DIR="/count-von-count"
+SCRIPT_DIR="${CVC_DIR}/lib/scripts"
 NGINX_DIR="/opt/openresty/nginx"
 
 rm -f $NGINX_DIR/conf/include/vars.conf
-echo 'set $redis_counter_hash '$(redis-cli SCRIPT LOAD "$(cat $DEPLOY_TO'/lib/redis/voncount.lua')")';' > $NGINX_DIR/conf/vars.conf
-redis-cli set von_count_config_live $(cat $DEPLOY_TO'/config/voncount.config' | tr -d '\n' | tr -d ' ')
-$NGINX_DIR/sbin/nginx -s reload
+echo 'set $redis_counter_hash '$(redis-cli SCRIPT LOAD "$(cat "${CVC_DIR}/lib/redis/voncount.lua")")';' > $NGINX_DIR/conf/vars.conf
+redis-cli set von_count_config_live $(cat "${CVC_DIR}/config/voncount.config" | tr -d '\n' | tr -d ' ')
+$NGINX_DIR/sbin/nginx -s reload || true
